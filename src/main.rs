f49b8f4f185f9bccbb0d0c11148353eb665dec61
Worik <5387413+worikgh@users.noreply.github.com>
2023-11-18 05:41:41 +0000

    Removed old commented code

--- src/main.rs
+++ src/main.rs
@@ -76,51 +76,6 @@ fn main() {
     for client in clients {
         client.deactivate().unwrap();
     }
-
-    // // Register ports. They will be used in a callback that will be
-    // // called when new data is available.
-    // let in_a = client
-    //     .register_port(port_in.as_str(), jack::AudioIn)
-    //     .unwrap();
-    // let mut out_a = client
-    //     .register_port(port_out.as_str(), jack::AudioOut)
-    //     .unwrap();
-
-    // // The channel to send audio data passing through this here to the
-    // // main thread
-    // let (tx, rx): (Sender<Vec<f32>>, Receiver<Vec<f32>>) = mpsc::channel::<Vec<f32>>();
-
-    // let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
-    //     //let tx = tx.clone();
-    //     let out_a_p: &mut [f32] = out_a.as_mut_slice(ps);
-    //     let in_a_p: &[f32] = in_a.as_slice(ps);
-    //     out_a_p.clone_from_slice(in_a_p);
-    //     tx.send(in_a_p.to_vec()).unwrap();
-    //     jack::Control::Continue
-    // };
-    // let process = jack::ClosureProcessHandler::new(process_callback);
-
-    // // Activate the client, which starts the processing.
-    // let active_client = client.activate_async(Notifications, process).unwrap();
-
-    // // Loop while JACK is getting, and sending, data
-    // loop {
-    //     let message: Vec<f32> = match rx.recv() {
-    //         Ok(m) => m,
-    //         Err(err) => {
-    //             eprintln!("{}", err);
-    //             break;
-    //         }
-    //     };
-
-    //     for v in message {
-    //         let bytes = v.to_ne_bytes();
-    //         writer.write_all(&bytes).unwrap();
-    //     }
-    //     writer.flush().unwrap();
-    // }
-
-    // active_client.deactivate().unwrap();
 }
 
 struct Notifications;
